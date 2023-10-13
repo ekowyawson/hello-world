a349857066b3bd6d533db15dc0885c75cbcad9a3
Ekow Yawson <147433440+ekowyawson@users.noreply.github.com>
2023-10-13 03:06:55 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,49 @@
-alert("Hey You!!");
-alert("You have stumbled on an awesome website about DNS! Exciting, right?!");
+// alert("Hey You! Guess what?");
+alert("You have stumbled upon this awesome website about DNS!\nExciting, right?!");
 
-let userInput = prompt("What does DNS stand for? For a million dollars, Alex...");
+function getUserInput() {
+    return prompt("What does DNS stand for?\nFor a million dollars, Alex ...");
+};
 
-document.write(`Awwww daaaamn! Someone may have just won a million dollars. Let's see what your answer was: [${userInput}]`);
+function evalUserInput() {
+    let result = getUserInput();
+    while (result === '' || result === null) {
+        alert(`You did not enter your aswer.\nLet's try again. You must enter an answer to move on.`);
+        result = getUserInput();
+    }
 
-let para = document.createElement('div');
-let userInputDiv = document.getElementById('user-input');
+    return result;
+};
 
-userInput = userInput.toLowerCase();
+// WRITE RESULTS TO DOM
+function revealResults() {
+    const userInput = evalUserInput();
+    const para = document.createElement('div');
+    const userInputDiv = document.getElementById('user-input');
+    userInputDiv.appendChild(para);
 
-if (userInput == "domain name service") {
-    para.textContent = `DING DING DING! We have a winner!`
-    para.style.color = 'green';
-} else {
-    para.textContent = `WOMP WOMP WOOOMP! Sorry, but you failed. Better luck next time.`
+    document.write(`Awwww shucks! Did you win? Let's see.\
+            <br>You answered: <span style="color:orange;">${userInput}<\span>`);
+
+    if (userInput.toLowerCase() == 'domain name system') {
+        para.textContent = `DING DING DING! We have a winner!\nYou will now be rewarded with a video!`;
+        para.style.color = 'green';
+        return;
+    }
+
+    para.textContent = `WOMP WOMP WOOOMP! Sorry, but you failed. Better luck next time.`;
     para.style.color = 'red';
+};
+
+function hideResults() {
+    // Hide created elements
+    const userInputClass = document.querySelector('.user-input');
+    setTimeout(() => {
+        // remove element from DOM
+        userInputClass.classList.add('hide');
+    }, 5000); // time in milliseconds
 }
 
-userInputDiv.appendChild(para);
+// LAZY CALL INIT FUNCTION
+revealResults();
+hideResults();
